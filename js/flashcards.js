afb03fb2ae5b628fcd4b5dc3ff8aa76fed1d54cb
morgunove <morgunove87@gmail.com>
2021-08-05 07:41:07 +0500

    production v 1.0

--- js/flashcards.js
+++ js/flashcards.js
@@ -1,5 +1,22 @@
-document.getElementById ( 'btnSave' ).addEventListener ( 'click', saveCard );
 let cards = [];
+
+init ();
+
+function init () {
+  document.getElementById ( 'btnSave' ).addEventListener ( 'click', saveCard );
+  getCard ();
+}
+
+function getCard () {
+  localforage.getItem ( 'flashcards' ).then ( function ( value ) {
+    if ( value != null ) {
+      cards = JSON.parse ( value );
+      document.getElementById ( 'numCards' ).innerHTML = cards.length;
+    }
+  } ).catch ( function ( err ) {
+    console.log ( err );
+  } )
+}
 
 function saveCard () {
   let frontContent = document.getElementById ( 'frontCard' ).value;
@@ -9,13 +26,25 @@ function saveCard () {
   console.log ( cards );
   clearUI ();
   numCardsOut ();
+  storeCards ();
 }
 
-numCardsOut = () => {
+function storeCards () {
+  let serializedCards = JSON.stringify ( cards );
+  localforage.setItem ( 'flashcards', serializedCards ).then ( function () {
+    return localforage.getItem ( 'key' );
+  } ).then ( function () {
+    alert ( 'saved card' );
+  } ).catch ( function ( err ) {
+    console.log ( 'error: ' + err );
+  } )
+}
+
+function numCardsOut () {
   document.getElementById ( 'numCards' ).innerHTML = cards.length;
 }
 
-clearUI = () => {
+function clearUI () {
   document.getElementById ( 'frontCard' ).value = '';
   document.getElementById ( 'backCard' ).value = '';
 }
